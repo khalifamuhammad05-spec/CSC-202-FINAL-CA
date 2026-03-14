addb6bef9fb9c527120404a169342dca91e7189b
khalifamuhammad05-spec <khalifamuhammad05@gmail.com>
2026-03-14 15:36:34 +0100

    changed the format for patient registration timestamp for cleaner display

--- models.py
+++ models.py
@@ -5,5 +5,5 @@ class Patient:
         self.name = name
         self.time_registered = datetime.now()
 
-    def display(self):
-        return f"{self.name} registered at {self.time_registered}"
+    def get_time(self):
+        return self.time_registered.strftime("%I:%M %p")
